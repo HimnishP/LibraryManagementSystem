f6dc86886ebfac608b720c8a5396eb5ce0414acd
Parth Patel <parthschool20s@gmail.com>
2024-11-25 17:32:24 -0500

    Added the LibraryInventory Class

---
+++ LibraryManagementSystem/LibraryInventory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    internal class LibraryInventory
+    {
+        protected List<Book> Books { get; set; }
+
+        public LibraryInventory(List<Book> books)
+        {
+            Books = new List<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            Books.Add(book);
+        }
+
+        public void RemoveBook(Book book)
+        {
+            if (Books.Contains(book))
+            {
+                Books.Remove(book);
+            }
+        }
+
+        // Search book method
+
+    }
+}
